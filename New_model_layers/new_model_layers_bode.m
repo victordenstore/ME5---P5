ce36ad4e982cb03d438cdd4f821d052678fecd16
wneum <wneum@192.168.1.8>
2021-11-23 19:16:14 +0100

    sdfs

--- New_model_layers/new_model_layers_bode.m
+++ New_model_layers/new_model_layers_bode.m
@@ -25,8 +25,10 @@ d_33 = 265*10^-12;
 h_33 = d_33/(s_33*eps_33);
 k_n_squared = h_33^2 *eps_33/c_33 * ones(length(omega),1);
 k_n_squared = k_n_squared';
-k_squared = k_n_squared/gamma;
-s = k_squared * sin(gamma)/gamma;
+k_squared = k_n_squared;
+gamma_sinus = sin(gamma);
+s_intermediate = k_squared.*gamma_sinus;
+s = s_intermediate./gamma;
 %gamma_mn = [pi*f/f_mn pi*f/f_backing];
 r = 2.5*10^-3; 
 S = pi*r^2;
@@ -35,7 +37,9 @@ Z_0 = rho_n*v_n*S;
 omega_mn = 2*pi*f_mn;
 Z_mn = Z_0;
 phi = sqrt(k_squared)*sqrt(omega_mn*c_mn*Z_mn/pi);
-c = k_squared * (1-cos(gamma))/gamma;
+gamma_cosinus = ones(1,length(gamma)) - cos(gamma);
+c_intermediate = k_squared .* gamma_cosinus;
+c = c_intermediate./gamma;
 A_mn = S;
 Q_mn = rho_n;
 Z_mn = A_mn * Q_mn; 
@@ -56,21 +60,23 @@ Kin_Vis=32*10^-6;
 %% 4 port model matrix
 
 
-T_11 = (cos(gamma) - s)/(1-s);
-T_12 = (j*Z_0*(sin(gamma) - 2*c))/(1-s);
-T_13 = -((cos(gamma) - 1)*phi)/(1-s);
+T_11 = (cos(gamma) - s)./(1-s);
+T_12 = (j*Z_0*(sin(gamma) - 2*c))./(1-s);
+cosinus_gamma = cos(gamma)-1;
+T_13 = -cosinus_gamma.*phi./(1-s);
 T_14 = 0;
-T_21 = (j*sin(gamma))/(Z_0*(1-s));
-T_22 = (cos(gamma) - s)/(1-s);
-T_23 = -(j*phi*sin(gamma))/(1-s);
+T_21 = (j*sin(gamma))./(Z_0*(1-s));
+T_22 = (cos(gamma) - s)./(1-s);
+T_23 = -(j*phi.*sin(gamma))./(1-s);
 T_24 = 0;
 T_31 = 0;
 T_32 = 0;
 T_33 = 1;
 T_34 = 0;
-T_41 = -(j*sin(gamma))/(Z_0*(1-s)) * phi;
-T_42 = -((cos(gamma) - 1)*phi)/(1-s);
-T_43 = (j*omega*C_0)/(1-s);
+T_41_intermediate = -(j*sin(gamma))./(Z_0*(1-s));
+T_41 = T_41_intermediate .* phi;
+T_42 = -((cos(gamma) - 1).*phi)./(1-s);
+T_43 = (j*omega*C_0)./(1-s);
 T_44 = 1;
 
 %% Intermediate layers (ex. glue layers, copper electrode, etceteraness)
@@ -80,7 +86,7 @@ A_copper = pi*r_copper^2;
 rho_copper = 8920;
 c_copper = 3570;
 d_copper = 0.5*10^-3;
-v_copper = sqrt(c_copper/rho_copper)
+v_copper = sqrt(c_copper/rho_copper);
 
 Z_copper = A_copper*rho_copper*c_copper;
 gamma_copper = omega * d_copper/v_copper;
@@ -99,7 +105,7 @@ r_FP = 4.5*10^-3;
 c_FP = 3230;
 S_FP = pi*r_FP^2;
 d_FP = 0.5*10^-3;
-v_FP = sqrt(c_FP/rho_FP)
+v_FP = sqrt(c_FP/rho_FP);
 
 Z_FP = rho_FP*c_FP*S_FP;
 gamma_FP = omega*d_FP/v_FP;
@@ -140,11 +146,11 @@ C_c(jj) = T_41(jj) - T_43(jj)*(T_21(jj)*Z_b(jj) + T_11(jj))/(T_23(jj)*Z_b(jj) + 
 D_c(jj) = T_42(jj) - T_43(jj)*(T_22(jj)*Z_b(jj) + T_12(jj))/(T_23(jj)*Z_b(jj) + T_13(jj));
 end
 
-A_c = zeros(2*length(omega),2);
+%A_C = zeros(2*length(omega),2);
 
 for x = 1:length(omega)
-    entry1 = 2*j - 1;
-    entry2 = 2*j;
+    entry1 = 2*x - 1;
+    entry2 = 2*x;
 A_C(entry1:entry2,1:2) = [A_c(x) B_c(x); C_c(x) D_c(x)]^2;
 end
 
@@ -186,18 +192,27 @@ Z_E(a) = (A(a)*Z_FP + B(a))/(C(a)*Z_FP + D(a));            % electrical input im
 V_IL(a) = Z_FP/(A(a)*Z_FP + B(a));                         % voltage transfer ratio between input voltage and output force
 end
 
-phase_impedance = angle(Z_E);
-phase_tf = angle(V_IL);
+phase_impedance = rad2deg(angle(Z_E));
+phase_tf = rad2deg(angle(V_IL));
 mag_impedance = 20*log10(abs(Z_E));
 mag_tf = 20*log10(abs(V_IL));
 
+[pks1 ind1] = findpeaks(mag_impedance,'MinPeakDistance',5000,'MinPeakProminence',0.01);
 figure
-subplot(2,1,1); plot(omega,mag_impedance);
-subplot(2,1,2); plot(omega,phase_impedance);
+subplot(2,1,1); plot(f(ind1),pks1,'or'); hold on; plot(f,mag_impedance); title('Bode magnitude plot of the impedance'); ...
+    xlabel('frequency [Hz]'); ylabel('magnitude [DB]');
+subplot(2,1,2); plot(f,phase_impedance); title('Phase plot of the impedance'); ...
+    xlabel('frequency [Hz]'); ylabel('phase [degree]');
+
+
+[pks ind] = findpeaks(mag_tf,'MinPeakDistance',5000,'MinPeakProminence',0.01);
 
 figure
-subplot(2,1,1); plot(omega,mag_tf);
-subplot(2,1,2); plot(omega,phase_tf);
+subplot(2,1,1); plot(f(ind),pks,'or'); hold on; plot(f,mag_tf); title('Bode magnitude plot of the transfer function V_{in} --> F_{out}'); ...
+    xlabel('frequency [Hz]'); ylabel('magnitude [DB]');
+subplot(2,1,2); plot(f,phase_tf); title('Phase plot of the transfer function V_{in} --> F_{out}'); ...
+    xlabel('frequency [Hz]'); ylabel('phase [degree]');
+
 
 
 
