0c88bca5f8d47e4d6c7c867e976048ffcf632f1e
wneum <wneum@172.27.83.218>
2021-11-25 13:04:15 +0100

    jnsdf

--- New_model_layers/New_model_layers.m
+++ New_model_layers/New_model_layers.m
@@ -17,13 +17,13 @@ f_mn = c_mn/(2*l_mn);                                                       % so
 %f_backing = c_backing/(2*l_backing);
 
 
-gamma = omega * d_n/v_n;                                                    % 
+gamma = pi*f*d_n;                                                    % omega * d_n/v_n
 eps_0=8.854*10^-12;          
 eps_33=1200*eps_0;
 s_33 = 14.2*10^-12;
 d_33 = 265*10^-12;
 h_33 = d_33/(s_33*eps_33);
-k_n_squared = h_33^2 *eps_33/c_33;
+k_n_squared = 0.66^2;                             % h_33^2 *eps_33/c_33
 k_squared = k_n_squared;
 s = k_squared * sin(gamma)/gamma;
 %gamma_mn = [pi*f/f_mn pi*f/f_backing];
@@ -34,10 +34,8 @@ Z_0 = rho_n*v_n*S;
 omega_mn = 2*pi*f_mn;
 Z_mn = Z_0;
 phi = sqrt(k_squared)*sqrt(omega_mn*c_mn*Z_mn/pi);
+
 c = k_squared * (1-cos(gamma))/gamma;
-A_mn = S;
-Q_mn = rho_n;
-Z_mn = A_mn * Q_mn; 
 
 rho_Back = 7850;
 c_Back = 3230;
@@ -76,7 +74,52 @@ Tn=[T_11 T_12 T_13 T_14; ...
     T_21 T_22 T_23 T_24; ...
     T_31 T_32 T_33 T_34; ...
     T_41 T_42 T_43 T_44];
-T=(Tn)^2;
+
+r_copper = 2.5*10^-3;
+A_copper = pi*r_copper^2;
+rho_copper = 8920;
+c_copper = 3570;
+d_copper = 0.5*10^-3;
+v_copper = sqrt(c_copper/rho_copper)
+
+Z_copper = A_copper*rho_copper*c_copper;
+gamma_copper = 0;
+
+A_copper = [cos(gamma_copper) j*Z_copper*sin(gamma_copper); ...
+    (j*sin(gamma_copper))/Z_copper cos(gamma_copper)];
+A_E = 1;
+B_E = 0;
+C_E = 0;
+D_E = 1;
+
+T_intermediate_copper = [A_copper(1,1) A_copper(1,2) 0 0; ...
+                  A_copper(2,1) A_copper(2,2) 0 0; ...
+                  0 0 A_E B_E; ...
+                  0 0 C_E D_E];
+              
+rho_FP = 7850;
+r_FP = 4.5*10^-3;
+c_FP = 3230;
+S_FP = pi*r_FP^2;
+d_FP = 0.5*10^-3;
+v_FP = sqrt(c_FP/rho_FP);
+
+Z_FP = rho_FP*c_FP*S_FP;
+gamma_FP = pi*f*d_FP;
+
+A_E_FP = 1;
+B_E_FP = 1/(omega*1400*eps_0*S_FP/d_FP);
+C_E_FP = 0;
+D_E_FP = 1;
+
+A_FP = [cos(gamma_FP) j*Z_FP*sin(gamma_FP); ...
+    (j*sin(gamma_FP))/Z_FP cos(gamma_FP)];
+T_intermediate_FP = [A_FP(1,1) A_FP(1,2) 0 0; ...
+                    A_FP(2,1) A_FP(2,2) 0 0; ...
+                    0 0 A_E_FP B_E_FP;
+                    0 0 C_E_FP D_E_FP];
+
+T=(Tn)^2 * T_intermediate_copper^2 * T_intermediate_FP;
 T_11=Tn(1,1);
 T_12=Tn(1,2);
 T_13=Tn(1,3);
@@ -93,33 +136,7 @@ T_41=Tn(4,1);
 T_42=Tn(4,2);
 T_43=Tn(4,3);
 T_44=Tn(4,4);
-%% Intermediate layers (ex. glue layers, copper electrode, etceteraness)
-
-r_copper = 2.5*10^-3;
-A_copper = pi*r_copper^2;
-rho_copper = 8920;
-c_copper = 3570;
-d_copper = 0.5*10^-3;
-v_copper = sqrt(c_copper/rho_copper)
-
-Z_copper = A_copper*rho_copper*c_copper;
-gamma_copper = omega * d_copper/v_copper;
-
-A_copper = [cos(gamma_copper) j*Z_copper*sin(gamma_copper); ...
-    (j*sin(gamma_copper))/Z_copper cos(gamma_copper)];
-
-rho_FP = 7850;
-r_FP = 4.5*10^-3;
-c_FP = 3230;
-S_FP = pi*r_FP^2;
-d_FP = 0.5*10^-3;
-v_FP = sqrt(c_FP/rho_FP)
-
-Z_FP = rho_FP*c_FP*S_FP;
-gamma_FP = omega*d_FP/v_FP;
-
-A_FP = [cos(gamma_FP) j*Z_FP*sin(gamma_FP); ...
-    (j*sin(gamma_FP))/Z_FP cos(gamma_FP)];
+%% Front and back layers (loaded impedance)
 
 rho_H = 7860;
 r_H = 2.5*10^-3;
@@ -127,18 +144,29 @@ c_H = 3230;
 S_H = pi*r_H^2;
 d_H = 3.25*10^-3;
 v_H = sqrt(c_H/rho_H);
-gamma_H = omega*d_H/v_H;
+gamma_H = pi*f*d_H;
 Z_H = rho_H*c_H*S_H;
 A_H = [cos(gamma_H) j*Z_H*sin(gamma_H); ...
     (j*sin(gamma_H))/Z_H cos(gamma_H)];
 
-A_m = A_copper*A_FP*A_H;
-A_b = A_m(1,1);
-B_b = A_m(1,2);
-C_b = A_m(2,1);
-D_b = A_m(2,2);
+rho_B = 7860;
+r_B = 2.5*10^-3;
+c_B = 3230;
+S_B = pi*r_B^2;
+d_B = 20*10^-3;
+v_B = sqrt(c_H/rho_H);
+gamma_B = pi*f*d_B;
+Z_B = rho_H*c_H*S_H;
+A_B = [cos(gamma_B) j*Z_H*sin(gamma_B); ...
+    (j*sin(gamma_B))/Z_B cos(gamma_B)];
 
-Z_b = (A_b*Z_NB + B_b)/(C_b*Z_NB + D_b);
+A_m = A_H*A_B;
+A_b = A_B(1,1);
+B_b = A_B(1,2);
+C_b = A_B(2,1);
+D_b = A_B(2,2);
+
+Z_b = (A_b*Z_B + B_b)/(C_b*Z_B + D_b);
 
 %% 2x2 matrices for parallel connection of piezoelectric layers
 
